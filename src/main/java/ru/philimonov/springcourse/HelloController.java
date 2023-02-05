3da91a4dc8ac16e3f341e6e8f5ed3af80276fb5b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-02-05 15:49:37 +0300

    first app initial commit

--- src/main/java/ru/philimonov/springcourse/HelloController.java
+++ src/main/java/ru/philimonov/springcourse/HelloController.java
@@ -1,2 +1,12 @@
-package ru.philimonov.springcourse;public class HelloController {
+package ru.philimonov.springcourse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayHello(){
+        return "hello_world";
+    }
 }
